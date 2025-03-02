342db042d9c8453aa6372d0674cf3ec7d8942ccc
antonioHigueron <acobosscabello@gmail.com>
2025-03-02 19:41:33 +0100

    permitir acceder a todos los servicios web

--- src/main/java/com/springboot/apirest/security/SecurityConfig.java
+++ src/main/java/com/springboot/apirest/security/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /*
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -30,11 +31,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .logoutUrl("/logout") // URL de logout
                 .logoutSuccessUrl("/login"); // Redirigir a login al cerrar sesión
+
+         */
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll() // Permitir acceso a todas las rutas sin autenticación
+                .and()
+                .formLogin().disable() // Deshabilitar el formulario de login de Spring Security
+                .logout()
+                .permitAll()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Configuración de usuario en memoria para pruebas (en un caso real sería desde una base de datos)
+        /*
         auth.inMemoryAuthentication()
                 .withUser("user")
                 .password("{noop}password") // {noop} es para que no se cifre la contraseña en pruebas
@@ -47,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("superadmin")
                 .password("{noop}superadmin")
                 .roles("ADMIN_SUPERIOR");
+        */
     }
 }
 
