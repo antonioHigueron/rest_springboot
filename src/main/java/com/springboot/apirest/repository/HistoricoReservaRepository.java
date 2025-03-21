6f2831614abb1498634281b7255c48a91e89b3cb
antonioHigueron <acobosscabello@gmail.com>
2025-03-21 08:08:06 +0100

    adaptaciones para mac

--- src/main/java/com/springboot/apirest/repository/HistoricoReservaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.apirest.repository;
-
-import com.springboot.apirest.dao.HistoricoReserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoricoReservaRepository extends JpaRepository<HistoricoReserva, Integer> {}
